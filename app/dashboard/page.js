e5217e261882c6f8beee166fac43dc843acae5e5
Zarivate <Zarivate@gmail.com>
2024-05-17 23:52:50 -0500

    Implemented loader then removed since redundant for this demo

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -12,42 +12,46 @@ import { ButtonInfoSection } from "@/components/buttonElement";
 
 const getToken = () => {
   const token = Cookies.get("token");
-
   return token;
 };
 
 const Dashboard = () => {
-  const [session, setSession] = useState();
+  const [session, setSession] = useState(false);
+  const [neutral, setNeutral] = useState(true);
 
   useEffect(() => {
     setSession(getToken());
   }, []);
 
+  // This still doesn't work so just forget it and go with the basics return ternary, remove the lase else
   // In order to prevent any hydration failures due to Nextjs pre rendering every page, which would cause an issue here due to rendering being dependant on whether the user
   // is signed in or not, a useEffect hook is used to set a session variable
-  if (session) {
+  if (session && neutral) {
     return (
       <DashWrap>
         <InfoSections {...dashObjOne} />
         <InfoSections {...dashObjTwo} />
       </DashWrap>
     );
+  } else if (neutral && !session) {
+    return (
+      <>
+        <OutterWrap>
+          <Image src={errorBg} alt="error Image" fill />
+          <Container>
+            <TopText>An account is needed to access this resource!</TopText>
+            <BtnTextWrapper>
+              <ButtonInfoSection as={Link} href="/signin" $err>
+                Create account
+              </ButtonInfoSection>
+            </BtnTextWrapper>
+          </Container>
+        </OutterWrap>
+      </>
+    );
+  } else {
+    return <div>Loading...</div>;
   }
-  return (
-    <>
-      <OutterWrap>
-        <Image src={errorBg} alt="error Image" fill />
-        <Container>
-          <TopText>An account is needed to access this resource!</TopText>
-          <BtnTextWrapper>
-            <ButtonInfoSection as={Link} href="/signin" $err>
-              Create account
-            </ButtonInfoSection>
-          </BtnTextWrapper>
-        </Container>
-      </OutterWrap>
-    </>
-  );
 };
 
 export default Dashboard;
